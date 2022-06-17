673ce2fa067d8bc4ff4080e63a25cd69f0a4e3c5
Jorge Montalvo <jorgemontalvo96@gmail.com>
2022-06-17 15:25:34 -0500

    factor: uso de inner.Text y comentarios

--- saludo/script.js
+++ saludo/script.js
@@ -1,20 +1,18 @@
 console.log("Todavía estoy aprendiendo")
 
-
-
 setInterval(cambiarSaludo, 2000);
 
-function cambiarSaludo() {
-  //console.log("Me parece una falta de respecto");
-  /* Estoy asignando a una variable el valor h2 */
-  let frase = document.getElementsByTagName("h2")[1].innerHTML;
+/**
+ * Cambia la frase en el título h2
+ */
 
-  if (document.getElementsByTagName("h2")[1].innerHTML == "Ganando como siempre") {
-    //frase = "Impongo moda para las niñas!";
-    document.getElementsByTagName("h2")[1].innerHTML = "Impongo moda para las niñas!";
+function cambiarSaludo() {
+ 
+  let frase = document.getElementsByTagName("h2")[1];
+  if (frase.innerText== "Ganando como siempre") {   
+    frase.innerText = "Impongo moda para las niñas!";
   } else {
-    //frase = "Ganando como siempre";
-    document.getElementsByTagName("h2")[1].innerHTML = "Ganando como siempre";
+    frase.innerText = "Ganando como siempre";
   }
   console.log("frase :" + frase);
 }
